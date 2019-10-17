43a3688125ffa1156326d0d8b5945710489dcb6b
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2019-10-17 10:29:24 +0200

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * This is a class
+ * Created 2019-10-17
+ *
+ * @author Magnus Silverdal
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+        JOptionPane.showMessageDialog(null,"Hello World");
+    }
+}
